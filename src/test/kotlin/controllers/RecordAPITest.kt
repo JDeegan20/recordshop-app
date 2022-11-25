4abe0027921dcdbe4371ea3df23a0438a787a9a0
JDeegan20 <20085768@mail.wit.ie>
2022-11-25 10:59:53 +0000

    Record Shop App - JUnit Tests for listing notes from the collection

--- src/test/kotlin/controllers/RecordAPITest.kt
+++ src/test/kotlin/controllers/RecordAPITest.kt
@@ -4,6 +4,7 @@ import models.Record
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -18,14 +19,14 @@ class RecordAPITest {
     private var emptyRecords: RecordAPI? = RecordAPI()
 
     @BeforeEach
-    fun setup(){
+    fun setup() {
         daftPunk = Record("Discovery", 50, "Electronic", false)
         pinkFloyd = Record("The Dark Side Of The Moon", 80, "Rock", false)
         fleetwoodMac = Record("Rumours", 40, "SoftRock", false)
         michaelJackson = Record("Thriller", 90, "Pop", false)
         dizzeeRascal = Record("Boy in da Corner", 30, "Grime", false)
 
-        //adding 5 Note to the notes api
+
         populatedRecords!!.add(daftPunk!!)
         populatedRecords!!.add(pinkFloyd!!)
         populatedRecords!!.add(fleetwoodMac!!)
@@ -34,9 +35,9 @@ class RecordAPITest {
     }
 
     @AfterEach
-    fun tearDown(){
+    fun tearDown() {
         daftPunk = null
-        pinkFloyd= null
+        pinkFloyd = null
         fleetwoodMac = null
         michaelJackson = null
         dizzeeRascal = null
@@ -44,21 +45,49 @@ class RecordAPITest {
         emptyRecords = null
     }
 
-    @Test
-    fun `adding a Record to a populated list adds to ArrayList`(){
-        val newRecord = Record("Marshall Mathers EP", 70, "Rap", false)
-        assertEquals(5, populatedRecords!!.numberOfRecords())
-        assertTrue(populatedRecords!!.add(newRecord))
-        assertEquals(6, populatedRecords!!.numberOfRecords())
-        assertEquals(newRecord, populatedRecords!!.findRecord(populatedRecords!!.numberOfRecords() - 1))
+    @Nested
+    inner class AddRecords {
+
+
+        @Test
+        fun `adding a Record to a populated list adds to ArrayList`() {
+            val newRecord = Record("Marshall Mathers EP", 70, "Rap", false)
+            assertEquals(5, populatedRecords!!.numberOfRecords())
+            assertTrue(populatedRecords!!.add(newRecord))
+            assertEquals(6, populatedRecords!!.numberOfRecords())
+            assertEquals(newRecord, populatedRecords!!.findRecord(populatedRecords!!.numberOfRecords() - 1))
+        }
+
+        @Test
+        fun `adding a Record to an empty list adds to ArrayList`() {
+            val newRecord = Record("Marshall Mathers EP", 70, "Rap", false)
+            assertEquals(0, emptyRecords!!.numberOfRecords())
+            assertTrue(emptyRecords!!.add(newRecord))
+            assertEquals(1, emptyRecords!!.numberOfRecords())
+            assertEquals(newRecord, emptyRecords!!.findRecord(emptyRecords!!.numberOfRecords() - 1))
+        }
     }
 
-    @Test
-    fun `adding a Record to an empty list adds to ArrayList`(){
-        val newRecord = Record("Marshall Mathers EP", 70, "Rap", false)
-        assertEquals(0, emptyRecords!!.numberOfRecords())
-        assertTrue(emptyRecords!!.add(newRecord))
-        assertEquals(1, emptyRecords!!.numberOfRecords())
-        assertEquals(newRecord, emptyRecords!!.findRecord(emptyRecords!!.numberOfRecords() - 1))
+
+    @Nested
+    inner class ListRecords {
+
+
+        @Test
+        fun `listAllRecords returns No Records Stored message when ArrayList is empty`() {
+            assertEquals(0, emptyRecords!!.numberOfRecords())
+            assertTrue(emptyRecords!!.listAllRecords().lowercase().contains("no records"))
+        }
+
+        @Test
+        fun `listAllRecords returns Records when ArrayList has records stored`() {
+            assertEquals(5, populatedRecords!!.numberOfRecords())
+            val recordsString = populatedRecords!!.listAllRecords().lowercase()
+            assertTrue(recordsString.contains("Daft Punk"))
+            assertTrue(recordsString.contains("Pink Floyd"))
+            assertTrue(recordsString.contains("Fleetwood Mac"))
+            assertTrue(recordsString.contains("Michael Jackson"))
+            assertTrue(recordsString.contains("Dizzee Rascal"))
+        }
     }
 }
