cd78d2ae5de2141c4fb16d5f672e14928904f6c0
JDeegan20 <20085768@mail.wit.ie>
2022-12-05 12:10:24 +0000

    Record Shop App - XML Serialization

--- src/test/kotlin/controllers/RecordAPITest.kt
+++ src/test/kotlin/controllers/RecordAPITest.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import persistence.XMLSerializer
+import java.io.File
 import kotlin.test.assertEquals
 
 class RecordAPITest {
@@ -15,8 +17,10 @@ class RecordAPITest {
     private var fleetwoodMac: Record? = null
     private var michaelJackson: Record? = null
     private var dizzeeRascal: Record? = null
-    private var populatedRecords: RecordAPI? = RecordAPI()
-    private var emptyRecords: RecordAPI? = RecordAPI()
+    private var populatedRecords: RecordAPI? =
+        RecordAPI(XMLSerializer(File("records.xml")))
+    private var emptyRecords: RecordAPI? =
+        RecordAPI(XMLSerializer(File("records.xml")))
 
     @BeforeEach
     fun setup() {
@@ -209,6 +213,47 @@ class RecordAPITest {
             assertEquals("Boy in Da Corner", populatedRecords!!.findRecord(4)!!.recordName)
             assertEquals(30, populatedRecords!!.findRecord(4)!!.recordCost)
             assertEquals("Electronic", populatedRecords!!.findRecord(4)!!.recordGenre)
+        }
+    }
+
+
+    @Nested
+    inner class PersistenceTests {
+
+        @Test
+        fun `saving and loading an empty collection in XML doesn't crash app`() {
+
+            val storingRecords = RecordAPI(XMLSerializer(File("records.xml")))
+            storingRecords.store()
+
+            val loadedRecords = RecordAPI(XMLSerializer(File("records.xml")))
+            loadedRecords.load()
+
+            assertEquals(0, storingRecords.numberOfRecords())
+            assertEquals(0, loadedRecords.numberOfRecords())
+            assertEquals(storingRecords.numberOfRecords(), loadedRecords.numberOfRecords())
+        }
+
+        @Test
+        fun `saving and loading an loaded collection in XML doesn't loose data`() {
+
+            val storingRecords = RecordAPI(XMLSerializer(File("records.xml")))
+            storingRecords.add(michaelJackson!!)
+            storingRecords.add(dizzeeRascal!!)
+            storingRecords.add(pinkFloyd!!)
+            storingRecords.store()
+
+
+            val loadedRecords = RecordAPI(XMLSerializer(File("records.xml")))
+            loadedRecords.load()
+
+
+            assertEquals(3, storingRecords.numberOfRecords())
+            assertEquals(3, storingRecords.numberOfRecords())
+            assertEquals(storingRecords.numberOfRecords(), loadedRecords.numberOfRecords())
+            assertEquals(storingRecords.findRecord(0), loadedRecords.findRecord(0))
+            assertEquals(storingRecords.findRecord(1), loadedRecords.findRecord(1))
+            assertEquals(storingRecords.findRecord(2), loadedRecords.findRecord(2))
         }
     }
 
